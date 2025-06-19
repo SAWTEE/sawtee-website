a5e0d91ad13fc96d2b41b08991d342f9a0a13278
Ankur <abishek.singh.chauhan@gmail.com>
2025-06-19 14:42:07 +0100

    Fix: newsletter pdf file in archive page.

--- resources/js/Pages/Frontend/Post.jsx
+++ resources/js/Pages/Frontend/Post.jsx
@@ -13,9 +13,9 @@ export default function Post({
 }) {
   const { category, title, content } = post;
   const isProgramme = category.parent && category.parent.slug === 'programme';
-  // const isNewsletter = category.slug === 'newsletters';
+  const isNewsletter = category.slug === 'newsletters';
   const isWebinarSeries = category.slug === 'webinar-series';
-  const isDefault = !isWebinarSeries;
+  const isDefault = !isWebinarSeries && !isNewsletter;
   const shareUrl = post.category.parent
     ? `https://info.sawtee.org/${post.category.parent.slug}/${post.category.slug}/${post.slug}`
     : `https://info.sawtee.org/${post.category.slug}/${post.slug}`;
@@ -40,10 +40,10 @@ export default function Post({
         featured_image={featured_image}
         srcSet={srcSet}
         isProgramPost={isProgramme}
-        // isNewsletter={isNewsletter}
+        isNewsletter={isNewsletter}
         relatedPosts={relatedPosts}
       >
-        {/* {isNewsletter && <NewsletterPost post={post} />} */}
+        {isNewsletter && <NewsletterPost post={post} />}
         {isWebinarSeries && <WebinarPost post={post} />}
         {isDefault && (
           <>
