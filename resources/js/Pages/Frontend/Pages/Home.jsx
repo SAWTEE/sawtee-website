78a61ce5c0de11b96561078abf111801cfded072
ManishDH1 <117730775+ManishDH1@users.noreply.github.com>
2025-08-08 15:44:58 +0545

    Update Home.jsx
    kept url in popup images

--- resources/js/Pages/Frontend/Pages/Home.jsx
+++ resources/js/Pages/Frontend/Pages/Home.jsx
@@ -87,6 +87,7 @@ const Home = ({
       {/* POPUP CODE */}
        <Dialog open={open} onOpenChange={() => setOpen(!open)}>
         <DialogContent className="p-0 bg-transparent shadow-none w-[800px] max-w-[90vw]">
+            <a href="https://sawtee.org/career" target="_blank" rel="noopener noreferrer">
           <DialogHeader>
               <img 
         src="https://info.sawtee.org/images/Website-02.jpg" 
